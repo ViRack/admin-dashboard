ffc0ac1b02a80c7d7c087c2e5885da62da9e7faa
ViRack <vincentm_2@hotmail.com>
2022-11-17 02:23:43 -0800

    hamburger complete. Need to work on desktop view.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav_menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -9,4 +9,8 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}))
+
+document.querySelectorAll("nav_link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove(".active");
 }))
